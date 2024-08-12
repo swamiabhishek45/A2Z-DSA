c1cb27f437cb907989fde1757868b19c3714188a
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-08-12 15:49:05 +0530

    isSorted done

--- Step 3 Arrays/Easy/03_isArraySorted.cpp
+++ Step 3 Arrays/Easy/03_isArraySorted.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Brute Force Approach --> O(n^2) | O(1)
+// Brute Force Approach --> O(n^2)
 bool isSorted(int arr[], int n){
     for(int i = 0; i<n; i++){
         for(int j = i+1; j<n; j++){
@@ -13,12 +13,22 @@ bool isSorted(int arr[], int n){
     return true;
 }
 
+// Optimal --> O(n)
+bool isArrSorted(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    int arr[]= {66,0,1,2,3,4,5,6,7};
+    int arr[]= {0,1,2,3,4,5,6,7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    bool ans = isSorted(arr, n);
+    bool ans = isArrSorted(arr, n);
 
     if(ans) cout << "True | Array is sorted" << endl;
     else cout << "False | Array is not sorted" << endl;
