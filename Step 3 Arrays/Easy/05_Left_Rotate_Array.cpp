c332c93e8b4c6967b333d118afa4863b8e744029
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-08-13 11:37:06 +0530

    add right rotation also

--- Step 3 Arrays/Easy/05_Left_Rotate_Array.cpp
+++ Step 3 Arrays/Easy/05_Left_Rotate_Array.cpp
@@ -19,11 +19,31 @@ void leftRotateArr(int arr[], int n)
     }
 }
 
+void rightRotateArr(int arr[], int n)
+{
+    int last = arr[n - 1];
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = last;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 int main()
 {
     int n = 5;
     int arr[n] = {1, 2, 3, 4, 5};
 
+    cout << "Left Rotation : ";
     leftRotateArr(arr, n);
+    cout << endl;
+    cout << "Right Rotation : ";
+    rightRotateArr(arr, n);
     return 0;
 }
